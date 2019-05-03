b78b3fddf0ea4de001d91cabef755d1b6512a319
Gabriel <quirschfeld@gmail.com>
2019-05-03 14:55:01 +0200

    remove dir

--- build/production/IJA-proj/get_libs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir res
-cd res
-wget --directory-prefix=./ --output-document=res.zip "https://drive.google.com/uc?export=download&id=1hr60Sq0t-JYoVmVxOsiHO-Pm658s4MAa"
-unzip res.zip
-rm res.zip
